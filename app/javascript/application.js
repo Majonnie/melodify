2286633d470e42daa1d3a8d3a944a2ef22181363
Majonnie <bonhommejulie2003@gmail.com>
2024-06-06 16:13:01 +0200

    [WIP] started using turbo with exercises, fixed js declaration in application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,6 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
+
+document.addEventListener("turbo:frame-load", function(event) {
+    console.log("Frame loaded!", event);
+});
