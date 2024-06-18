a028111286e71158cfeaa126dd648eca465c5242
Majonnie <bonhommejulie2003@gmail.com>
2024-06-18 11:40:01 +0200

    [FEAT] improved header (not logged in) and theme switch design

--- app/javascript/theme_controller.js
+++ app/javascript/theme_controller.js
@@ -2,16 +2,26 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 
+    icons = {"light": "fa-moon", "dark": "fa-sun"};
+    static targets = [ "themeIcon" ];
     connect() {
         const currentTheme = localStorage.getItem('theme') || 'light'; // Default to light mode
-        document.body.className = currentTheme;
+        this.toggleIcon(this.themeIconTarget, null, currentTheme);
+        document.body.classList.add(currentTheme);
     }
 
-    //static targets = [ "themeName" ];
-
     toggleTheme() {
-        const newTheme = document.body.className === 'light' ? 'dark' : 'light';
-        document.body.className = newTheme;
+        const oldTheme = document.body.classList.contains('light') ? 'light' : 'dark';
+        const newTheme = document.body.classList.contains('light') ? 'dark' : 'light';
+        console.log();
+        this.toggleIcon(this.themeIconTarget, oldTheme, newTheme);
+        document.body.classList.remove(oldTheme);
+        document.body.classList.add(newTheme);
         localStorage.setItem('theme', newTheme);
+    }
+
+    toggleIcon(icon, oldTheme, newTheme) {
+        icon.classList.add(this.icons[newTheme]);
+        icon.classList.remove(this.icons[oldTheme]);
     }
 }
